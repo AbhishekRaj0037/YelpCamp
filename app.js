ff89541adb52095bf2dd67829a5ac0b3ab0835d9
AbhishekRaj0037 <me_abhishek.tethered@icloud.com>
2022-09-06 02:27:00 +0530

    ok

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.use((err, req, res, next) => {
 
 })
 
-
-app.listen(3000, () => {
-    console.log('server running 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server running ${port}`)
 })
